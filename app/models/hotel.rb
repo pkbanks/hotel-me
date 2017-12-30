d3f7f3445db54c710ee04e96256bcef263a38841
PK Banks <pkbanks12@gmail.com>
2017-12-30 12:36:25 -0500

    added hirb gem for rails console, and create seed file

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,7 @@
 # app/models/hotel.rb
 class Hotel < ApplicationRecord
 
+  # require name, location
+  validates :name, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Hotel.create(name:"exlora Chile", location:"Patagonia chile")
+
+peninsula = Hotel.new
+peninsula.name = "the peninsula hongkong"
+peninsula.location = "hong kong"
+peninsula.save
+
+Hotel.create(name: "Four Seasons Whistler", location: "Whistler, Canada")
+Hotel.create(name: "Motel 8", location: "Waukesha, WI")
+
+Hotel.create(name: "B Ocean", location: "Lake Buena Vista, FL")
+puts "Hotel: B Ocean created"
+
+meridien = Hotel.new
+meridien.name = "Le Meridien"
+
+Hotel.create(name: "Ritz Carlton", location: "Grove, Miami")
+
+Hotel.create(name:"Setai Miami", location:"Miami, Florida")
+
+puts "#{Hotel.count} hotels created."
